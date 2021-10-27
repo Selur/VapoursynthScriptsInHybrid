a1dceebcf50115b84d704a152115900159cb0999
Georg Pelz <hybrid@selur.de>
2021-10-27 18:31:38 +0200

    hysteria core adjustment vor Vapoursynth 55+

--- hysteria.py
+++ hysteria.py
@@ -70,7 +70,7 @@
 #
 # 9/11/10: Is this thing on?
 # 10/4/15: Port to VapourSynth by Overdrive80
-# 6/13/17: removed dependency, fixed parameter scaling and mask mode is now "cartoon" by BluBb_mADe
+# 6/13/17: removed dependency, fixed parameter scaling and masking by BluBb_mADe
 #
 ###################
 
@@ -78,64 +78,65 @@
 import vapoursynth as vs
 
 
-def Hysteria(clip, strength= 1.0, usemask=True, lowthresh=6, highthresh=20, luma_cap=191, maxchg=255, minchg=0, planes = [0], luma=True, showmask=False):
-
-    core = vs.get_core()
+def Hysteria(clip, strength=1.0, usemask=True, lowthresh=6, highthresh=20, luma_cap=191, maxchg=255, minchg=0,
+             planes=[0], luma=True, showmask=False):
+    core = vs.core
     if not isinstance(clip, vs.VideoNode):
         raise ValueError('This is not a clip')
 
-    max_bitval = (1<<clip.format.bits_per_sample) - 1
-    
+    max_bitval = (1 << clip.format.bits_per_sample) - 1
+
     def scale(old_value):
-        return int((old_value * (max_bitval)) / 255)
-    
+        return int((old_value * max_bitval) / 255)
+
     # This scales the colordepth dependant parameters
     if clip.format.bits_per_sample != 8:
         lowthresh = scale(lowthresh)
         highthresh = scale(highthresh)
-        
+
         luma_cap = scale(luma_cap)
-        
+
         maxchg = scale(maxchg)
         minchg = scale(minchg)
 
-
     # Medium value
-    mid =  (2 ** clip.format.bits_per_sample) // 2
-    
-    # imitate mt_edge(mode=cartoon) (stolen from Frechdachs)
-    noisymask = core.std.Convolution(clip,matrix=[0,-2,1,0,1,0,0,0,0], planes=planes, saturate=True)
-    noisymask = core.std.Expr(noisymask, ['x {high} >= {maxvalue} x {low} <= 0 x ? ?'
-                                           .format(low=lowthresh, high=lowthresh, maxvalue=max_bitval), ''])
-    
-    cleanmask = core.std.Convolution(clip,matrix=[0,-2,1,0,1,0,0,0,0], planes=planes, saturate=True)
-    cleanmask = core.std.Expr(cleanmask, ['x {high} >= {maxvalue} x {low} <= 0 x ? ?'
-                                           .format(low=highthresh, high=highthresh, maxvalue=max_bitval), ''])
+    mid = (2 ** clip.format.bits_per_sample) // 2
 
-    themask = core.misc.Hysteresis(cleanmask,noisymask)
+    # imitate mt_edge(mode=cartoon) (stolen from Frechdachs)
+    noisymask = core.std.Convolution(clip, matrix=[0, -2, 1, 0, 1, 0, 0, 0, 0], planes=planes, saturate=True)
+    noisymask = core.std.Expr(noisymask, ['x {high} >= {maxvalue} x {low} <= 0 x ? ?'
+                              .format(low=lowthresh, high=lowthresh, maxvalue=max_bitval)])
+
+    cleanmask = core.std.Convolution(clip, matrix=[0, -2, 1, 0, 1, 0, 0, 0, 0], planes=planes, saturate=True)
+    cleanmask = core.std.Expr(cleanmask, ['x {high} >= {maxvalue} x {low} <= 0 x ? ?'
+                              .format(low=highthresh, high=highthresh, maxvalue=max_bitval)])
+
+    themask = core.misc.Hysteresis(cleanmask, noisymask)
     themask = core.std.Inflate(themask)
-    
+
     # blur replacement
-    themask = core.std.Convolution(themask,matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
-    themask = core.std.Convolution(themask,matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
-    
+    themask = core.std.Convolution(themask, matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
+    themask = core.std.Convolution(themask, matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
+
     themask = core.std.Deflate(themask)
 
     clipa = core.std.Inflate(clip, planes=[0])
-    diffs = core.std.MakeDiff(clipa,clip)
+    diffs = core.std.MakeDiff(clipa, clip)
     diffs = core.std.Expr([diffs], ['x {mid} - {strength} *'.format(strength=strength, mid=mid)])
 
-    darkened = core.std.Expr([clip, diffs], ['x x {luma_cap} > 0 y {maxchg} > {maxchg} y {minchg} < 0 y ? ? ? -'.format(luma_cap=luma_cap,maxchg=maxchg,minchg=minchg)])
+    darkened = core.std.Expr([clip, diffs], [
+        'x x {luma_cap} > 0 y {maxchg} > {maxchg} y {minchg} < 0 y ? ? ? -'.format(luma_cap=luma_cap, maxchg=maxchg,
+                                                                                   minchg=minchg)])
 
     if usemask:
         themask = core.std.ShufflePlanes(themask, planes=[0], colorfamily=vs.GRAY)
-        final = core.std.MaskedMerge(clip,darkened,themask, planes=planes, first_plane=luma)
+        final = core.std.MaskedMerge(clip, darkened, themask, planes=planes, first_plane=luma)
     else:
-        final = core.std.ShufflePlanes(clips=[darkened,clip], planes=[0, 1, 2], colorfamily=vs.YUV)
+        final = core.std.ShufflePlanes(clips=[darkened, clip], planes=[0, 1, 2], colorfamily=vs.YUV)
 
     if not showmask:
         return final
-    
+
     mascara = core.std.Levels(themask, min_in=0, max_in=max_bitval, min_out=scale(80), max_out=max_bitval)
-    mascara = core.std.ShufflePlanes([mascara,clip], planes=[0,1,2], colorfamily=vs.YUV)
+    mascara = core.std.ShufflePlanes([mascara, clip], planes=[0, 1, 2], colorfamily=vs.YUV)
     return mascara
