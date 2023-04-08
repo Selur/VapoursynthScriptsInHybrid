fd7ba1d3a084d905049ad1fa97ed61152211d988
Georg Pelz <hybrid@selur.de>
2023-04-08 15:27:00 +0200

    replace gmfss_union with gmfss_fortuna in filldrops

--- filldrops.py
+++ filldrops.py
@@ -28,15 +28,15 @@ def fillWithRIFE(clip, firstframe=None, rifeModel=1, rifeTTA=False, rifeUHD=Fals
   return core.std.AssumeFPS(join, src=clip)
 
 
-def fillWithGMFSSUnion(clip, firstframe=None, gmfsuModel=0, gmfsuThresh=0.15):
-  from vsgmfss_union import gmfss_union
+def fillWithGMFSSUnion(clip, firstframe=None, gmfssModel=0, gmfssThresh=0.15):
+  from vsgmfss_fortuna import gmfss_fortuna
   clip1 = core.std.AssumeFPS(clip, fpsnum=1, fpsden=1)
   start = core.std.Trim(clip1, first=firstframe-1, length=1)
   end = core.std.Trim(clip1, first=firstframe+1, length=1)
   startend = start + end
   if clip.format != vs.RGBH:
     r = core.resize.Point(startend, format=vs.RGBH, matrix_in_s="709")
-  r = gmfss_union(r, model=gmfsuModel, sc_threshold=gmfsuThresh)
+  r = gmfss_fortuna(r, model=gmfssModel, sc_threshold=gmfssThresh)
   if clip.format != vs.RGBH:
     r = core.resize.Point(r, format=clip.format, matrix_s="709")
   r = core.std.Trim(r, first=1, last=1) 
@@ -68,7 +68,7 @@ def fillWithSVP(clip, firstframe=None, gpu=False):  # Here I go wrong since I se
   join = a + r + b
   return core.std.AssumeFPS(join, src=clip)
     
-def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, rifeUHD=False, rifeThresh=0.15, gmfsuModel=0, gmfsuThresh=0.15, debug=False):
+def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, rifeUHD=False, rifeThresh=0.15, gmfssModel=0, gmfssThresh=0.15, debug=False):
   core = vs.core
   if not isinstance(clip, vs.VideoNode):
       raise ValueError('This is not a clip')
@@ -87,8 +87,8 @@ def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, r
         filldrops=fillWithSVP(clip,n,gpu=True)
       elif method == "rife":
         filldrops = fillWithRIFE(clip,n,rifeModel,rifeTTA,rifeUHD,rifeThresh)
-      elif method == "gmfssunion":
-        filldrops = fillWithGMFSSUnion(clip,n,gmfsuModel,gmfsuThresh)
+      elif method == "gmfssfortuna":
+        filldrops = fillWithGMFSSUnion(clip,n,gmfssModel,gmfssThresh)
       else:
         raise vs.Error('FillDrops: Unknown method '+method)   
     if debug:
