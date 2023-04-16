b0618567e3de32603a3bc367eb3382665ff95c46
Georg Pelz <hybrid@selur.de>
2023-04-16 13:18:06 +0200

    Added a port of of AviSynth TFMBobN(..) by jagabo, see: https://forum.videohelp.com/threads/409273-QTGMC-shimmer-when-deinterlacing-a-duplicate-frame#post2687062

---
+++ TFMBobN.py
@@ -0,0 +1,25 @@
+import vapoursynth as vs
+core = vs.core
+
+# port of AviSynth TFMBobN(..) by jagabo, see: https://forum.videohelp.com/threads/409273-QTGMC-shimmer-when-deinterlacing-a-duplicate-frame#post2687062
+def TFMBobN(clip: vs.VideoNode,
+            pp: int = 6,
+            cthresh: int = 9,
+            MI: int = 80,
+            openCL: bool = False):
+  field = clip.get_frame(0).props['_FieldBased']
+  if openCL:
+    n = core.nnedi3cl.NNEDI3CL(clip = clip, field=field,nns=4)
+  else:
+    n = core.nnedi3.nnedi3(clip=clip, field=field, nns=4)
+  if field == 1: #BFF
+    switchedField = 2
+  else:  #TFF
+    switchedField = 1
+  odd = core.tivtc.TFM(clip=clip, field=switchedField, clip2=n[::2], PP=pp, cthresh=cthresh, MI=MI)
+  even = core.tivtc.TFM(clip=clip, field=switchedField, clip2=n[1::2], PP=pp, cthresh=cthresh, MI=MI)
+  return core.std.Interleave([odd,even])
+
+
+
+
