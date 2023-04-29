71ec8139eaeb2e5b2cdcc05cf902236b4539e30f
Georg Pelz <hybrid@selur.de>
2023-04-29 13:44:56 +0200

    chromashiftsp: indention + comment

--- chromashift.py
+++ chromashift.py
@@ -36,17 +36,19 @@ def ChromaShiftSP (clip, X=0.0, Y=0.0, shiftU=True, shiftV=True):
 	#Vapoursynth version of Avisynth ChromaShiftSP
 	#Original AVS ChromaShift_SP: Shift chroma with subpixel accuracy, basic function by IanB, made standalone by McCauley
 	#X: positive values shift the chroma to left, negative values to right
-	#Y: positive values shift the chroma upwards, negative values downwards
-	Yplane = core.std.ShufflePlanes(clip, planes=0, colorfamily=vs.GRAY)
-	shift = core.resize.Spline16(clip, width=clip.width, height=clip.height, src_left=X, src_top=Y, src_width=clip.width + X, src_height=clip.height + Y)
-  if !shiftU and !shiftV:
+  #Y: positive values shift the chroma upwards, negative values downwards
+  #shifU: shift U plane
+  #shifV: shift V plane
+  Yplane = core.std.ShufflePlanes(clip, planes=0, colorfamily=vs.GRAY)
+  shift = core.resize.Spline16(clip, width=clip.width, height=clip.height, src_left=X, src_top=Y, src_width=clip.width + X, src_height=clip.height + Y)
+  if not shiftU and  not shiftV:
     raise vs.Error('ChromaShiftSP: at least U or V needs to be shifted!')
   if shiftU and shiftV:
 	  merge = core.std.ShufflePlanes(clips=[Yplane, shift], planes=[0, 1, 2], colorfamily=vs.YUV)
   elif shiftU:
     Vplane = core.std.ShufflePlanes(clip, planes=2, colorfamily=vs.GRAY)
-    merge = core.std.ShufflePlanes(clips=[Yplane, shift, Vplane], planes=[0, 1, 2], colorfamily=vs.YUV)
+    merge = core.std.ShufflePlanes(clips=[Yplane, shift, Vplane], planes=[0, 1, 0], colorfamily=vs.YUV)
   elif shiftV:
     Uplane = core.std.ShufflePlanes(clip, planes=2, colorfamily=vs.GRAY)
-    merge = core.std.ShufflePlanes(clips=[Yplane, Uplane, shift], planes=[0, 1, 2], colorfamily=vs.YUV)
-	return merge
+    merge = core.std.ShufflePlanes(clips=[Yplane, Uplane, shift], planes=[0, 0, 2], colorfamily=vs.YUV)
+  return merge
