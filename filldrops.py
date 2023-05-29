b36ecd1390288285c4b502eacbba9327e39a2c23
Georg Pelz <hybrid@selur.de>
2023-05-29 12:04:01 +0200

    ReplaceBlackFrames: replace a single black frame with last frame that was not black FillBlackFrames: replace a black frames with interpolations of the surrounding frames.

--- filldrops.py
+++ filldrops.py
@@ -48,7 +48,7 @@ def fillWithGMFSSUnion(clip, firstframe=None, gmfssModel=0, gmfssThresh=0.15):
 
 
 
-def fillWithSVP(clip, firstframe=None, gpu=False):  # Here I go wrong since I select the wrong frames 
+def fillWithSVP(clip, firstframe=None, gpu=False):
   clip1 = core.std.AssumeFPS(clip, fpsnum=1, fpsden=1)
   start = core.std.Trim(clip1, first=firstframe-1, length=1)
   end = core.std.Trim(clip1, first=firstframe+1, length=1)
@@ -98,3 +98,95 @@ def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, r
   diffclip = core.std.PlaneStats(clip, clip[0] + clip)
   fixed = core.std.FrameEval(clip, selectFunc, prop_src=diffclip)
   return fixed
+
+
+# replace a single black frame with last frame that was not black
+def ReplaceBlackFrames(clip, thresh=0.1, debug=False):
+  core = vs.core
+  if not isinstance(clip, vs.VideoNode):
+      raise ValueError('This is not a clip')
+      
+  def selectFunc(n, f):
+    if f.props['PlaneStatsAverage'] > thresh or n == 0:
+      if debug:
+        return core.text.Text(clip=clip,text="Org, avg: "+str(f.props['PlaneStatsAverage']),alignment=8)
+      return clip
+    toReplace = n
+    for i in range(n)[::-1]:
+      if clip.get_frame(i).props['PlaneStatsAverage'] <= thresh:
+        continue
+      
+      # remove current replace with frame before    
+      start = core.std.Trim(clip, first=0, last=n-1) 
+      r = core.std.Trim(clip, first=i, last=i)
+      end = core.std.Trim(clip, first=n+1) 
+      replaced = start + r + end
+      break
+    
+    if debug:
+       return core.text.Text(clip=replaced,text="Replaced, avg: "+str(f.props['PlaneStatsAverage']),alignment=8)
+    return replaced
+          
+  clip = core.std.PlaneStats(clip)
+  fixed = core.std.FrameEval(clip, selectFunc, prop_src=clip)
+  return fixed
+ 
+# replace the frames 'firstFrametoReplace' to 'firstGoodFrame-1' with interpolations between 'firstFrametoReplace - 1' and 'firstGoodFrame'
+def fillMultipleWithSVP(clip, firstFrametoReplace=None, firstGoodFrame=None, gpu=False):
+  clip1 = core.std.AssumeFPS(clip, fpsnum=1, fpsden=1)
+  start = core.std.Trim(clip1, first=firstFrametoReplace-1, length=1) # last good frame before black frames
+  end = core.std.Trim(clip1, first=firstGoodFrame, length=1) # first good frame after black frames
+  startend = start + end
+  if gpu:
+    super  = core.svp1.Super(startend,"{gpu:1}")
+  else:
+    super  = core.svp1.Super(startend,"{gpu:0}")
+  vectors= core.svp1.Analyse(super["clip"],super["data"],startend,"{}")
+
+  # interpolate
+  r = core.svp2.SmoothFps(startend,super["clip"],super["data"],vectors["clip"],vectors["data"],"{rate:{num:"+str(firstGoodFrame-firstFrametoReplace+1)+",den:1,abs:false}}")
+  a = core.std.Trim(clip1, first=0, last=firstFrametoReplace-2) # last good frame before is part of r
+  b = core.std.Trim(clip1, first=firstGoodFrame+1) # first good frame, ist part of r
+  # join
+  join = a + r + b
+  if (join.num_frames != clip.num_frames): # did I messup with the join
+    raise vs.Error(f"fillMultipleWithSVP: frame count issue join '{join.num_frames}' vs clip '{clip.num_frames}'")
+  return core.std.AssumeFPS(join, src=clip)
+  
+
+# replace a black frames with interpolations of the surrounding frames.
+# threshold average frame threshold
+def FillBlackFrames(clip, thresh=0.1, debug=False):
+    if not isinstance(clip, vs.VideoNode):
+        raise ValueError('This is not a clip')
+    
+    def selectFunc(n, f):
+        nonlocal clip
+        firstGoodFrame = f.props['FirstGoodFrame']
+        if n == 0:
+          clip = core.std.SetFrameProp(clip, prop="FirstGoodFrame", intval=firstGoodFrame)
+          return clip
+        if firstGoodFrame >= n: # this frame, we already dealt with
+            return clip
+
+        if f.props['PlaneStatsAverage'] > thresh or n == 0:
+            if debug:
+                return core.text.Text(clip=clip, text="Org, avg: " + str(f.props['PlaneStatsAverage']), alignment=8)
+            clip = core.std.SetFrameProp(clip, prop="FirstGoodFrame", intval=n)
+            return clip
+        firstFrametoReplace = n
+        firstGoodFrame = n + 1
+        for i in range(n, clip.num_frames-1):
+            if clip.get_frame(i).props['PlaneStatsAverage'] <= thresh:
+                continue
+            firstGoodFrame = i
+            break
+        clip = core.std.SetFrameProp(clip, prop="FirstGoodFrame", intval=firstGoodFrame)
+        replaced = fillMultipleWithSVP(clip, firstFrametoReplace, firstGoodFrame, gpu=True)
+        if debug:
+            return core.text.Text(clip=replaced, text="Replaced from " + str(firstFrametoReplace) + " to " + str(firstGoodFrame-1))
+        return replaced
+    clip = core.std.SetFrameProp(clip, prop="FirstGoodFrame", intval=0)
+    clip = core.std.PlaneStats(clip)
+    fixed = core.std.FrameEval(clip, selectFunc, prop_src=clip)
+    return fixed
