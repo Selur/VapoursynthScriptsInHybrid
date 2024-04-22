6bc59c5ab5ac580c223d3a0f96e72c24bfd770b7
Georg Pelz <hybrid@selur.de>
2024-04-22 15:34:05 +0200

    move SCDetect out of FameEval, assume YUVX input

--- filldrops.py
+++ filldrops.py
@@ -7,19 +7,14 @@ def fillWithMVTools(clip):
   vbe = core.mv.Analyse(super, truemotion=True, isb=True, delta=1)
   return core.mv.FlowInter(clip, super, mvbw=vbe, mvfw=vfe, time=50)
     
-def fillWithRIFE(clip, firstframe=None, rifeModel=1, rifeTTA=False, rifeUHD=False, rifeThresh=0.15):
+def fillWithRIFE(clip, firstframe=None, rifeModel=1, rifeTTA=False, rifeUHD=False):
   clip1 = core.std.AssumeFPS(clip, fpsnum=1, fpsden=1)
   start = core.std.Trim(clip1, first=firstframe-1, length=1)
   end = core.std.Trim(clip1, first=firstframe+1, length=1)
   startend = start + end
-  adjustColor = clip.format.id != vs.RGBS
-  if adjustColor:
-    startend = core.resize.Bicubic(startend, format=vs.RGBS, matrix_in_s="709")
-  if rifeThresh != 0:
-    startend = core.misc.SCDetect(clip=startend,threshold=rifeThresh)
+  startend = core.resize.Bicubic(startend, format=vs.RGBS, matrix_in_s="709")
   r = core.rife.RIFE(startend, model=rifeModel, tta=rifeTTA, uhd=rifeUHD)
-  if adjustColor:
-    r = core.resize.Bicubic(r, format=clip.format, matrix_s="709")
+  r = core.resize.Bicubic(r, format=clip.format, matrix_s="709")
   r = core.std.Trim(r, first=1, last=1) 
   r = core.std.AssumeFPS(r, fpsnum=1, fpsden=1)
   a = core.std.Trim(clip1, first=0, last=firstframe-1) 
@@ -54,11 +49,9 @@ def fillWithGMFSSUnion(clip, firstframe=None, gmfssModel=0, gmfssThresh=0.15):
   start = core.std.Trim(clip1, first=firstframe-1, length=1)
   end = core.std.Trim(clip1, first=firstframe+1, length=1)
   startend = start + end
-  if clip.format != vs.RGBH:
-    r = core.resize.Bicubic(startend, format=vs.RGBH, matrix_in_s="709")
+  r = core.resize.Bicubic(startend, format=vs.RGBH, matrix_in_s="709")
   r = gmfss_fortuna(r, model=gmfssModel, sc_threshold=gmfssThresh)
-  if clip.format != vs.RGBH:
-    r = core.resize.Bicubic(r, format=clip.format, matrix_s="709")
+  r = core.resize.Bicubic(r, format=clip.format, matrix_s="709")
   r = core.std.Trim(r, first=1, last=1) 
   r = core.std.AssumeFPS(r, fpsnum=1, fpsden=1)
   a = core.std.Trim(clip1, first=0, last=firstframe-1) 
@@ -73,7 +66,7 @@ def fillWithSVP(clip, firstframe=None, gpu=False):
   start = core.std.Trim(clip1, first=firstframe-1, length=1)
   end = core.std.Trim(clip1, first=firstframe+1, length=1)
   startend = start + end
-  
+ 
   if gpu:
     super  = core.svp1.Super(startend,"{gpu:1}")
   else:
@@ -91,8 +84,14 @@ def fillWithSVP(clip, firstframe=None, gpu=False):
 def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, rifeUHD=False, rifeThresh=0.15, gmfssModel=0, gmfssThresh=0.15, debug=False):
   core = vs.core
   if not isinstance(clip, vs.VideoNode):
-      raise ValueError('This is not a clip')
-      
+    raise ValueError('This is not a clip')
+
+  if clip.format.color_family != vs.YUV:
+    raise ValueError('FillSingleDrops requires YUV input')
+    
+  if method == "rife" and rifeThresh != 0:
+    clip = core.misc.SCDetect(clip=clip,threshold=rifeThresh)
+    
   def selectFunc(n, f):
     if f.props['PlaneStatsDiff'] > thresh or n == 0:
       if debug:
@@ -106,7 +105,7 @@ def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, r
       elif method == "svp_gpu":
         filldrops=fillWithSVP(clip,n,gpu=True)
       elif method == "rife":
-        filldrops = fillWithRIFE(clip,n,rifeModel,rifeTTA,rifeUHD,rifeThresh)
+        filldrops = fillWithRIFE(clip,n,rifeModel,rifeTTA,rifeUHD)
       elif method == "gmfssfortuna":
         filldrops = fillWithGMFSSUnion(clip,n,gmfssModel,gmfssThresh)
       else:
@@ -122,8 +121,14 @@ def FillSingleDrops(clip, thresh=0.3, method="mv", rifeModel=0, rifeTTA=False, r
 def InsertSingle(clip, afterEveryX=2, method="mv", rifeModel=0, rifeTTA=False, rifeUHD=False, rifeThresh=0.15, gmfssModel=0, gmfssThresh=0.15, debug=False):
   core = vs.core
   if not isinstance(clip, vs.VideoNode):
-      raise ValueError('This is not a clip')
-      
+    raise ValueError('This is not a clip')
+    
+  if clip.format.color_family != vs.YUV:
+    raise ValueError('InsertSingle requires YUV input')  
+    
+  if method == "rife" and rifeThresh != 0:
+    clip = core.misc.SCDetect(clip=clip,threshold=rifeThresh)
+       
   def selectFunc(n):
     if n == 0 or n%afterEveryX != 0:
       return clip
@@ -135,7 +140,7 @@ def InsertSingle(clip, afterEveryX=2, method="mv", rifeModel=0, rifeTTA=False, r
       elif method == "svp_gpu":
         insertFrame=fillWithSVP(clip,n,gpu=True)
       elif method == "rife":
-        insertFrame = fillWithRIFE(clip,n,rifeModel,rifeTTA,rifeUHD,rifeThresh)
+        insertFrame = fillWithRIFE(clip,n,rifeModel,rifeTTA,rifeUHD)
       elif method == "gmfssfortuna":
         insertFrame = fillWithGMFSSUnion(clip,n,gmfssModel,gmfssThresh)
       else:
@@ -149,7 +154,10 @@ def ReplaceSingle(clip, frameList, method="mv", rifeModel=0, rifeTTA=False, rife
     core = vs.core
     if not isinstance(clip, vs.VideoNode):
         raise ValueError('This is not a clip')
-
+    if clip.formatcolor_family != vs.YUV:
+       raise ValueError('ReplaceSingle requires YUV input')
+    if method == "rife" and rifeThresh != 0:
+       clip = core.misc.SCDetect(clip=clip,threshold=rifeThresh)
     def selectFunc(n):
       if n in frameList:
         if method == "mv":
@@ -159,7 +167,7 @@ def ReplaceSingle(clip, frameList, method="mv", rifeModel=0, rifeTTA=False, rife
         elif method == "svp_gpu":
             insertFrame = fillWithSVP(clip, n, gpu=True)
         elif method == "rife":
-            insertFrame = fillWithRIFE(clip, n, rifeModel, rifeTTA, rifeUHD, rifeThresh)
+            insertFrame = fillWithRIFE(clip, n, rifeModel, rifeTTA, rifeUHD)
         elif method == "gmfssfortuna":
             insertFrame = fillWithGMFSSUnion(clip, n, gmfssModel, gmfssThresh)
         else:
