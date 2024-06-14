06105300fdbd1eb66e00ae52c406af204e209070
Georg Pelz <hybrid@selur.de>
2024-06-14 22:07:11 +0200

    Adjusted FillDuplicateFrames to incomporate FillDrops and ReplaceSingle.

--- FillDuplicateFrames.py
+++ FillDuplicateFrames.py
@@ -17,11 +17,12 @@ v0.0.3
 0.0.4 removed RGBH since RIFE ncnn does not support it
 0.0.5 add general sceneThr
 0.0.6 add rifeModel parameter
+0.0.7 add mode: FillDuplicate|FillDrops|Replace
 '''
 
 class FillDuplicateFrames:
   # constructor
-  def __init__(self, clip: vs.VideoNode, thresh: float=0.001, method: str='SVP', debug: bool=False, sceneThr: float=0.15, rifeModel: int=22, device_index: int=0):
+  def __init__(self, clip: vs.VideoNode, mode='FillDuplicate', thresh: float=0.001, method: str='SVP', sceneThr: float=0.15, rifeModel: int=22, frames = [], debug: bool=False, device_index: int=0):
       # calculte stats
       self.thresh = thresh
       self.debug = debug
@@ -31,11 +32,23 @@ class FillDuplicateFrames:
       self.device_index = device_index
       self.rifeModel = rifeModel
       self.clip = core.std.PlaneStats(clip, clip[0]+clip)
-      if sceneThr > 0 and method == 'RIFE':
+      self.mode = mode
+      self.frames = frames
+      if sceneThr > 0 and method.lower() == 'rife':
         clip = core.misc.SCDetect(clip=clip,threshold=sceneThr)
-          
+      if method == 'Replace' and not frames:
+        raise ValueError(f'FillDuplicateFrames: "frames" needs to be set when using \'{self.method}\'!')
+
   def interpolate(self, n, f):
-    out = self.get_current_or_interpolate(n)
+    if self.mode == 'FillDuplicate':
+      out = self.get_current_or_interpolate(n)
+    elif self.mode == 'FillDrops':
+      out = self.get_current_or_interpolate_for_fill(n)
+    elif self.mode == 'Replace':
+      out = self.replaceFrame(n)
+    else:
+      raise ValueError(f'FillDuplicateFrames: Unknown mode \'{self.mode}\'!')
+      
     if self.debug:
       return out.text.Text(text="avg: "+str(f.props['PlaneStatsDiff']),alignment=8)            
     return out
@@ -67,7 +80,7 @@ class FillDuplicateFrames:
   def interpolateWithSVP(self, clip, n, start, end):   
     if clip.format.id != vs.YUV420P8:
       raise ValueError(f'FillDuplicateFrames: "clip" needs to be YUV420P8 when using \'{self.method}\'!')
-    if self.method == 'SVP':
+    if self.method.lower() == 'svp_gpu' or self.method == 'SVP':
       super = core.svp1.Super(clip,"{gpu:1}")
     else: # self.method == 'SVPCPU':
       super = core.svp1.Super(clip,"{gpu:0}")
@@ -83,7 +96,6 @@ class FillDuplicateFrames:
   
   def get_current_or_interpolate(self, n):
     if self.is_not_duplicate(n):
-    
       if self.potential_scene_change(n):
         if self.debug:
           return self.clip[n].text.Text(text="Input (scene change - 1)", alignment=9)
@@ -124,14 +136,120 @@ class FillDuplicateFrames:
     #interpolating two frame clip  into end-start+1 fps
     clip = self.clip[start] + self.clip[end]
     clip = clip.std.AssumeFPS(fpsnum=1, fpsden=1)
-    if self.method == 'SVP' or self.method == 'SVPcpu':  
+    if self.method.lower() == 'svp' or self.method == 'SVPcpu' or self.method == 'svp_gpu':  
       return self.interpolateWithSVP(clip, n, start, end)
     elif self.method == 'RIFE':
       return self.interpolateWithRIFE(clip, n, start, end)
     elif self.method == 'MV':
       return self.interpolateWithMV(clip, n, start, end)
     else:
-      raise ValueError(f'FillDuplicateFrames: "method" \'{self.method}\' is not supported atm.')
+      raise ValueError(f'FillDuplicateFrames: {self.mode} "method" \'{self.method}\' is not supported atm.')
+      
+  def get_current_or_interpolate_for_fill(self, n):
+    if n == 0 or n >= self.clip.num_frames -1:
+      if self.debug:
+          return self.clip[n].text.Text(text="Input (0)", alignment=9)
+      return self.clip[n]
+    if self.is_not_duplicate(n):
+      if self.potential_scene_change(n):
+        if self.debug:
+          return self.clip[n].text.Text(text="Input (scene change - 1)", alignment=9)
+      elif self.debug:
+        #current non dublicate selected
+        return self.clip[n].text.Text(text="Input (1)", alignment=9)
+      return self.clip[n]
+    
+    start = n-1
+    # previous frame is duplicate => nothing can be done
+    if not self.is_not_duplicate(start):
+      if self.debug:
+        return self.clip[n].text.Text(text="Input (2)", alignment=9)
+      return self.clip[n]
+    # previous frame is scene change => nothing can be done
+    if self.potential_scene_change(start):        
+      if self.debug:
+        return self.clip[n].text.Text(text="Input(before scene change)", alignment=9)
+      return self.clip[n]
+        
+    end = n+1
+    # next frame is duplicate => nothing can be done
+    if not self.is_not_duplicate(start):
+      if self.debug:
+        return self.clip[n].text.Text(text="Input (2)", alignment=9)
+      return self.clip[n]
+    # nex frame is scene change => nothing can be done
+    if self.potential_scene_change(start):        
+      if self.debug:
+        return self.clip[n].text.Text(text="Input(before scene change)", alignment=9)
+      return self.clip[n]
+
+    #does interpolated smooth clip exist for requested n frame? Use n frame from it.
+    if self.smooth is not None and start >= self.smooth_start and end <= self.smooth_end:
+      if self.debug:
+        return self.smooth[n-start].text.Text(text=self.method, alignment=9)
+      return self.smooth[n-start]
+
+    #interpolating two frame clip  into end-start+1 fps
+    clip = self.clip[start] + self.clip[end]
+    clip = clip.std.AssumeFPS(fpsnum=1, fpsden=1)
+    if self.method == 'svp_gpu' or self.method == 'svp':  
+      return self.interpolateWithSVP(clip, n, start, end)
+    elif self.method.lower() == 'rife':
+      return self.interpolateWithRIFE(clip, n, start, end)
+    elif self.method.lower() == 'mv':
+      return self.interpolateWithMV(clip, n, start, end)
+    else:
+      raise ValueError(f'FillDuplicateFrames: {self.mode} "method" \'{self.method}\' is not supported atm.')
+
+  def replaceFrame(self, n):
+   
+    if not n in self.frames or n == 0 or n >= self.clip.num_frames -1:
+      if self.debug:
+          return self.clip[n].text.Text(text="Input (0)", alignment=9)
+      return self.clip[n]
+      
+    start = n-1
+    # previous frame is duplicate => nothing can be done
+    if not self.is_not_duplicate(start):
+      if self.debug:
+        return self.clip[n].text.Text(text="Input (1)", alignment=9)
+      return self.clip[n]
+    # previous frame is scene change => nothing can be done
+    if self.potential_scene_change(start):        
+      if self.debug:
+        return self.clip[n].text.Text(text="Input(before scene change)", alignment=9)
+      return self.clip[n]
+        
+    end = n+1
+    # next frame is duplicate => nothing can be done
+    if not self.is_not_duplicate(start):
+      if self.debug:
+        return self.clip[n].text.Text(text="Input (2)", alignment=9)
+      return self.clip[n]
+    # nex frame is scene change => nothing can be done
+    if self.potential_scene_change(start):        
+      if self.debug:
+        return self.clip[n].text.Text(text="Input(before scene change)", alignment=9)
+      return self.clip[n]
+
+    #does interpolated smooth clip exist for requested n frame? Use n frame from it.
+    if self.smooth is not None and start >= self.smooth_start and end <= self.smooth_end:
+      if self.debug:
+        return self.smooth[n-start].text.Text(text=self.method, alignment=9)
+      return self.smooth[n-start]
+
+    #interpolating two frame clip  into end-start+1 fps
+    clip = self.clip[start] + self.clip[end]
+    clip = clip.std.AssumeFPS(fpsnum=1, fpsden=1)
+    if self.method == 'svp_gpu' or self.method == 'svp':  
+      return self.interpolateWithSVP(clip, n, start, end)
+    elif self.method.lower() == 'rife':
+      return self.interpolateWithRIFE(clip, n, start, end)
+    elif self.method.lower() == 'mv':
+      return self.interpolateWithMV(clip, n, start, end)
+    else:
+      raise ValueError(f'FillDuplicateFrames: {self.mode} "method" \'{self.method}\' is not supported atm.')
+
 
   def is_not_duplicate(self, n):
     return self.clip.get_frame(n).props['PlaneStatsDiff'] > self.thresh
