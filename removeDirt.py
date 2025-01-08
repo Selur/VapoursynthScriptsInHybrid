62d276714d27d4f598573e0c4c06dc5870112af1
Georg Pelz <hybrid@selur.de>
2025-01-08 19:17:06 +0100

    support RemoveDirt port by pinterf https://github.com/pinterf/RemoveDirt

--- removeDirt.py
+++ removeDirt.py
@@ -10,10 +10,21 @@ def RemoveDirt(input, repmode=16, remgrainmode=17, limit=10):
   cleansed = core.rgvs.Clense(input)
   sbegin = core.rgvs.ForwardClense(input)
   send = core.rgvs.BackwardClense(input)
-  scenechange = core.rdvs.SCSelect(input, sbegin, send, cleansed)
+  if hasattr(core, 'removedirt') and hasattr(core.removedirt, 'SCSelect'):
+    # Use removedirt.SCSelect if available
+    scenechange = core.removedirt.SCSelect(input, sbegin, send, cleansed)
+  else:
+    # Fallback to rdvs.SCSelect
+    scenechange = core.rdvs.SCSelect(input, sbegin, send, cleansed)
   alt = core.rgvs.Repair(scenechange, input, mode=[repmode,repmode,1])
   restore = core.rgvs.Repair(cleansed, input, mode=[repmode,repmode,1])
-  corrected = core.rdvs.RestoreMotionBlocks(cleansed, restore, neighbour=input, alternative=alt, gmthreshold=70, dist=1, dmode=2, noise=limit, noisy=12)
+  
+  if hasattr(core, 'removedirt') and hasattr(core.removedirt, 'SCSelect'):
+    # Use removedirt.RestoreMotionBlocks if available
+    corrected = core.removedirt.RestoreMotionBlocks(cleansed, restore, neighbour=input, alternative=alt, gmthreshold=70, dist=1, dmode=2, noise=limit, noisy=12)
+  else:
+    # Fallback to rdvs.RestoreMotionBlocks
+    corrected = core.rdvs.RestoreMotionBlocks(cleansed, restore, neighbour=input, alternative=alt, gmthreshold=70, dist=1, dmode=2, noise=limit, noisy=12)
   return core.rgvs.RemoveGrain(corrected, mode=[remgrainmode,remgrainmode,1])
   
 def RemoveDirtMC(input, limit=6, repmode=16, remgrainmode=17, block_size=8, block_over = 4, gpu=False):
