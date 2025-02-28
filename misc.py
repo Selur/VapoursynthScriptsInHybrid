4360a7777c8d7e73e6767b1dd2206bdc466452be
Georg Pelz <hybrid@selur.de>
2025-02-28 06:39:30 +0100

    working on removing havsfunc

---
+++ misc.py
@@ -0,0 +1,170 @@
+from vapoursynth import core
+import vapoursynth as vs
+
+from typing import Optional, Union, Sequence
+
+def Overlay(
+    base: vs.VideoNode,
+    overlay: vs.VideoNode,
+    x: int = 0,
+    y: int = 0,
+    mask: Optional[vs.VideoNode] = None,
+    opacity: float = 1.0,
+    mode: str = 'normal',
+    planes: Optional[Union[int, Sequence[int]]] = None,
+    mask_first_plane: bool = True,
+) -> vs.VideoNode:
+    '''
+    Puts clip overlay on top of clip base using different blend modes, and with optional x,y positioning, masking and opacity.
+
+    Parameters:
+        base: This clip will be the base, determining the size and all other video properties of the result.
+
+        overlay: This is the image that will be placed on top of the base clip.
+
+        x, y: Define the placement of the overlay image on the base clip, in pixels. Can be positive or negative.
+
+        mask: Optional transparency mask. Must be the same size as overlay. Where mask is darker, overlay will be more transparent.
+
+        opacity: Set overlay transparency. The value is from 0.0 to 1.0, where 0.0 is transparent and 1.0 is fully opaque.
+            This value is multiplied by mask luminance to form the final opacity.
+
+        mode: Defines how your overlay should be blended with your base image. Available blend modes are:
+            addition, average, burn, darken, difference, divide, dodge, exclusion, extremity, freeze, glow, grainextract, grainmerge, hardlight, hardmix, heat,
+            lighten, linearlight, multiply, negation, normal, overlay, phoenix, pinlight, reflect, screen, softlight, subtract, vividlight
+
+        planes: Specifies which planes will be processed. Any unprocessed planes will be simply copied.
+
+        mask_first_plane: If true, only the mask's first plane will be used for transparency.
+    '''
+    if not (isinstance(base, vs.VideoNode) and isinstance(overlay, vs.VideoNode)):
+        raise vs.Error('Overlay: this is not a clip')
+
+    if mask is not None:
+        if not isinstance(mask, vs.VideoNode):
+            raise vs.Error('Overlay: mask is not a clip')
+
+        if mask.width != overlay.width or mask.height != overlay.height or mask.format.bits_per_sample != overlay.format.bits_per_sample :
+            raise vs.Error('Overlay: mask must have the same dimensions and bit depth as overlay')
+
+    if base.format.sample_type == vs.INTEGER:
+        bits = base.format.bits_per_sample 
+        neutral = 1 << (bits - 1)
+        peak = (1 << bits) - 1
+        factor = 1 << bits
+    else:
+        neutral = 0.5
+        peak = factor = 1.0
+
+    plane_range = range(base.format.num_planes)
+
+    if planes is None:
+        planes = list(plane_range)
+    elif isinstance(planes, int):
+        planes = [planes]
+
+    if base.format.subsampling_w > 0 or base.format.subsampling_h > 0:
+        base_orig = base
+        base = base.resize.Point(format=base.format.replace(subsampling_w=0, subsampling_h=0))
+    else:
+        base_orig = None
+
+    if overlay.format.id != base.format.id:
+        overlay = overlay.resize.Point(format=base.format)
+
+    if mask is None:
+        mask = overlay.std.BlankClip(format=overlay.format.replace(color_family=vs.GRAY, subsampling_w=0, subsampling_h=0), color=peak)
+    elif mask.format.id != overlay.format.id and mask.format.color_family != vs.GRAY:
+        mask = mask.resize.Point(format=overlay.format, range_s='full')
+
+    opacity = min(max(opacity, 0.0), 1.0)
+    mode = mode.lower()
+
+    # Calculate padding sizes
+    l, r = x, base.width - overlay.width - x
+    t, b = y, base.height - overlay.height - y
+
+    # Split into crop and padding values
+    cl, pl = min(l, 0) * -1, max(l, 0)
+    cr, pr = min(r, 0) * -1, max(r, 0)
+    ct, pt = min(t, 0) * -1, max(t, 0)
+    cb, pb = min(b, 0) * -1, max(b, 0)
+
+    # Crop and padding
+    overlay = overlay.std.Crop(left=cl, right=cr, top=ct, bottom=cb)
+    overlay = overlay.std.AddBorders(left=pl, right=pr, top=pt, bottom=pb)
+    mask = mask.std.Crop(left=cl, right=cr, top=ct, bottom=cb)
+    mask = mask.std.AddBorders(left=pl, right=pr, top=pt, bottom=pb, color=[0] * mask.format.num_planes)
+
+    if opacity < 1:
+        mask = mask.std.Expr(expr=f'x {opacity} *')
+
+    if mode == 'normal':
+        pass
+    elif mode == 'addition':
+        expr = f'x y +'
+    elif mode == 'average':
+        expr = f'x y + 2 /'
+    elif mode == 'burn':
+        expr = f'x 0 <= x {peak} {peak} y - {factor} * x / - ?'
+    elif mode == 'darken':
+        expr = f'x y min'
+    elif mode == 'difference':
+        expr = f'x y - abs'
+    elif mode == 'divide':
+        expr = f'y 0 <= {peak} {peak} x * y / ?'
+    elif mode == 'dodge':
+        expr = f'x {peak} >= x y {factor} * {peak} x - / ?'
+    elif mode == 'exclusion':
+        expr = f'x y + 2 x * y * {peak} / -'
+    elif mode == 'extremity':
+        expr = f'{peak} x - y - abs'
+    elif mode == 'freeze':
+        expr = f'y 0 <= 0 {peak} {peak} x - dup * y / {peak} min - ?'
+    elif mode == 'glow':
+        expr = f'x {peak} >= x y y * {peak} x - / ?'
+    elif mode == 'grainextract':
+        expr = f'x y - {neutral} +'
+    elif mode == 'grainmerge':
+        expr = f'x y + {neutral} -'
+    elif mode == 'hardlight':
+        expr = f'y {neutral} < 2 y x * {peak} / * {peak} 2 {peak} y - {peak} x - * {peak} / * - ?'
+    elif mode == 'hardmix':
+        expr = f'x {peak} y - < 0 {peak} ?'
+    elif mode == 'heat':
+        expr = f'x 0 <= 0 {peak} {peak} y - dup * x / {peak} min - ?'
+    elif mode == 'lighten':
+        expr = f'x y max'
+    elif mode == 'linearlight':
+        expr = f'y {neutral} < y 2 x * + {peak} - y 2 x {neutral} - * + ?'
+    elif mode == 'multiply':
+        expr = f'x y * {peak} /'
+    elif mode == 'negation':
+        expr = f'{peak} {peak} x - y - abs -'
+    elif mode == 'overlay':
+        expr = f'x {neutral} < 2 x y * {peak} / * {peak} 2 {peak} x - {peak} y - * {peak} / * - ?'
+    elif mode == 'phoenix':
+        expr = f'x y min x y max - {peak} +'
+    elif mode == 'pinlight':
+        expr = f'y {neutral} < x 2 y * min x 2 y {neutral} - * max ?'
+    elif mode == 'reflect':
+        expr = f'y {peak} >= y x x * {peak} y - / ?'
+    elif mode == 'screen':
+        expr = f'{peak} {peak} x - {peak} y - * {peak} / -'
+    elif mode == 'softlight':
+        expr = f'x {neutral} > y {peak} y - x {neutral} - * {neutral} / 0.5 y {neutral} - abs {peak} / - * + y y {neutral} x - {neutral} / * 0.5 y {neutral} - abs {peak} / - * - ?'
+    elif mode == 'subtract':
+        expr = f'x y -'
+    elif mode == 'vividlight':
+        expr = f'x {neutral} < x 0 <= 2 x * {peak} {peak} y - {factor} * 2 x * / - ? 2 x {neutral} - * {peak} >= 2 x {neutral} - * y {factor} * {peak} 2 x {neutral} - * - / ? ?'
+    else:
+        raise vs.Error('Overlay: invalid mode specified')
+
+    if mode != 'normal':
+        overlay = core.std.Expr([overlay, base], expr=[expr if i in planes else '' for i in plane_range])
+
+    # Return padded clip
+    last = core.std.MaskedMerge(base, overlay, mask, planes=planes, first_plane=mask_first_plane)
+    if base_orig is not None:
+        last = last.resize.Point(format=base_orig.format)
+    return last
