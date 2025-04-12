ff40687059c8a52d8caa612ed9f5afcddab28f6b
Georg Pelz <hybrid@selur.de>
2025-04-12 16:41:36 +0200

    *added* RemoveDirtSMC

--- removeDirt.py
+++ removeDirt.py
@@ -1,12 +1,12 @@
 import vapoursynth as vs
+from vapoursynth import core
+
 # dependencies:
 # RemoveGrain (http://www.vapoursynth.com/doc/plugins/rgvs.html) or zsmooth (https://github.com/adworacz/zsmooth)
 # MVTools (https://github.com/dubhater/vapoursynth-mvtools) or SVP dlls when gpu=True is used
 # RemoveDirt (https://github.com/pinterf/removedirtvs, https://github.com/Rational-Encoding-Thaumaturgy/vapoursynth-removedirt)
 # ChangeFPS (https://github.com/Selur/VapoursynthScriptsInHybrid/blob/master/ChangeFPS.py)
-
-def RemoveDirt(input, repmode=16, remgrainmode=17, limit=10):
-  core = vs.core
+def RemoveDirt(input: vs.VideoNode, repmode: int=16, remgrainmode: int=17, limit: int=10) -> vs.VideoNode:
   cleansed = core.rgvs.Clense(input)
   sbegin = core.rgvs.ForwardClense(input)
   send = core.rgvs.BackwardClense(input)
@@ -30,8 +30,7 @@ def RemoveDirt(input, repmode=16, remgrainmode=17, limit=10):
   else:
     return core.rgvs.RemoveGrain(corrected, mode=[remgrainmode,remgrainmode,1])
   
-def RemoveDirtMC(input, limit=6, repmode=16, remgrainmode=17, block_size=8, block_over = 4, gpu=False):
-  core = vs.core
+def RemoveDirtMC(input: vs.VideoNode, limit: int=6, repmode: int=16, remgrainmode: int=17, block_size: int=8, block_over: int=4, gpu: bool=False) -> vs.VideoNode:
   if hasattr(core, 'zsmooth'):
     quad = core.zsmooth.RemoveGrain(input, mode=[12,0,1])   # blur the luma for searching motion vectors  orig avs: mode=12, modeU=-1
   else:
@@ -60,3 +59,52 @@ def RemoveDirtMC(input, limit=6, repmode=16, remgrainmode=17, block_size=8, bloc
   clp = RemoveDirt(clp, repmode=2, remgrainmode=17, limit=limit)
   clp = core.std.SelectEvery(clp,3,1)
   return clp
+
+
+def RemoveTempGrain(clip: vs.VideoNode, mode: int=17) -> vs.VideoNode:
+  if hasattr(core, 'zsmooth'):
+    rg = core.zsmooth.RemoveGrain(clip, mode=mode)
+  else:
+    rg = core.rgvs.RemoveGrain(clip, mode=mode)
+  return TemporalRepair(rg, clip)
+
+def RemoveDirtS(clip: vs.VideoNode, limit: int= 6, grey: bool=False) -> vs.VideoNode:
+  clensed = core.rgvs.Clense(clip)
+  if hasattr(core, 'zsmooth'):
+    alt = core.zsmooth.RemoveGrain(clip, mode=10)
+  else:
+    alt = core.rgvs.RemoveGrain(clip, mode=10)
+  return core.removedirt.RestoreMotionBlocks(clensed, clip, alternative=alt, pthreshold=4, cthreshold=6, gmthreshold=40, dist=1, dmode=2, debug=False, noise=limit, noisy=16, grey=False)
+
+def RemoveDirtSMC(clip: vs.VideoNode, limit: int=6, repmode: int=16, remgrainmode: int=17, block_size: int=8, block_over: int=4, gpu: bool=False) -> vs.VideoNode:
+  
+  if gpu:
+    import ChangeFPS
+    block_over = 0 if block_over == 0 else 1 if block_over == 2 else 2 if block_over == 4 else 3 
+    Super = core.svp1.Super(clip, "{gpu:1,pel:4}")
+    bvec = core.svp1.Analyse(Super['clip'], Super['data'], clip, "{ gpu:1, block:{w:"+str(block_size)+", h:"+str(block_size)+",overlap:"+str(block_over)+"} }")
+    fvec = core.svp1.Analyse(Super['clip'], Super['data'], clip, "{ gpu:1, block:{w:"+str(block_size)+", h:"+str(block_size)+",overlap:"+str(block_over)+",special:{delta: 1}} }")
+    backw = core.svp2.SmoothFps(clip,Super['clip'], Super['data'],bvec['clip'],bvec['data'],"{}") # here the frame rate is doubled
+    forw = core.svp2.SmoothFps(clip,Super['clip'], Super['data'],fvec['clip'],fvec['data'],"{}")  # here the frame rate is doubled
+    # since backw and forw now have twice the frame count I drop half the frames
+    backw = ChangeFPS.ChangeFPS(backw,clip.fps_num,clip.fps_den)
+    forw = ChangeFPS.ChangeFPS(forw,clip.fps_num,clip.fps_den)
+  else:
+    #block size of MAnalyze, blksize 8 is much better for 720x576 noisy source than blksize=16 
+    #block overlapping of MAnalyze 0! 2 or 4 is not good for my noisy b&w 8mm film source
+    i = core.mv.Super(clip, pel=2, sharp=2)    #  avs: i=MSuper(clip,pel=2, isse=false)
+    bvec = core.mv.Analyse(super=i,isb=True, blksize=block_size,overlap=block_over, delta=1, truemotion=True, chroma=True)  
+    fvec = core.mv.Analyse(super=i,isb=False, blksize=block_size,overlap=block_over, delta=1, truemotion=True, chroma=True)
+    backw = core.mv.Flow(clip=clip,super=i,vectors=[bvec])
+    forw  = core.mv.Flow(clip=clip,super=i,vectors=[fvec])
+
+  clip    = core.std.Interleave([backw, clip, forw])
+  clip    = RemoveDirtS(clip, limit)
+  if hasattr(core, 'zsmooth'):
+    clip = core.zsmooth.RemoveGrain(clip, mode=10)
+  else:
+    clip = core.rgvs.RemoveGrain(clip, mode=10)
+    
+  clip = core.std.SelectEvery(clip, 3, 1)
+ 
+  return clip
